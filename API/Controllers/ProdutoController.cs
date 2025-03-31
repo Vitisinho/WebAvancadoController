ed25e08ecae020079f77781b16d25ae3c9d52db0
VITOR MARCELO BARZICK NOGUEIRA <vitor.nogueira@cs.up.edu.br>
2025-03-31 20:05:39 -0300

    first commit

---
+++ API/Controllers/ProdutoController.cs
@@ -0,0 +1,21 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers;
+
+[ApiController]
+[Route("api/produto")]
+public class ProdutoController : ControllerBase
+{
+    //Exemplo de metodo dentro de uma classe
+    public string Metodo()
+    {
+        return "";
+    }
+
+    //Exemplo de um EndPoint dentro de um Contrller
+    [HttpGet("helloworld")]
+    public IActionResult HelloWorld()
+    {
+        return Ok("Hello World!");
+    }
+}
