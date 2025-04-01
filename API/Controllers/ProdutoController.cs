0ebb76858139a2fc18a55b403c8b3a3a336f179e
VITOR MARCELO BARZICK NOGUEIRA <vitor.nogueira@cs.up.edu.br>
2025-03-31 21:42:36 -0300

    criacao do endpoint de cadastro de produto no banco

--- API/Controllers/ProdutoController.cs
+++ API/Controllers/ProdutoController.cs
@@ -1,3 +1,5 @@
+using API.Data;
+using API.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace API.Controllers;
@@ -6,6 +8,20 @@ namespace API.Controllers;
 [Route("api/produto")]
 public class ProdutoController : ControllerBase
 {
+    private readonly AppDataContext _context;
+    public ProdutoController(AppDataContext contexto)
+    {
+        _context = contexto;
+    }
+
+    [HttpPost("cadastrar")]
+    public IActionResult Cadastrar([FromBody] Produto produto)
+    {
+        _context.Produtos.Add(produto);
+        _context.SaveChanges();
+        return Created("", produto);
+    }
+
     //Exemplo de metodo dentro de uma classe
     [HttpGet]
     public string Metodo()
--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f0b3932c19b8127429b0a49f96510778815008fe")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+495293b5c369542023b75a785944c698ed430f47")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
